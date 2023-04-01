b8732eeabf67a5d7dc39feb6ab1cda3a2818bd9b
Khuong Nguyen <khuong.nguyen@saritasa.com>
2023-04-01 15:40:00 +0700

    Update material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModuleModule } from 'src/theme/material-module/material-module.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MaterialModuleModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/theme/material-module/material-module.module.ts
+++ src/theme/material-module/material-module.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MaterialModuleComponent } from './material-module.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 
+const MATERIAL_MODULE = [
+  MatButtonModule,
+  MatCheckboxModule,
+  MatCardModule,
+  MatDialogModule
+]
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [MaterialModuleComponent]
+  exports: [...MATERIAL_MODULE],
+
 })
 export class MaterialModuleModule { }
